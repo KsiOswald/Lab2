ba266897b20ebd82c1deb975547b9aea7b0b7c0f
KsiOswald <plahin06@gmail.com>
2026-03-02 01:10:12 +0500

    Лабораторная работа

---
+++ lab2.1.fs
@@ -0,0 +1,34 @@
+﻿open System
+
+//Получить список из первых цифр чисел, содержащихся в исходном списке
+
+// Функция ввода
+let input N = [ 
+    for i in 1..N do
+        printf "Введите число: "
+        let numb = int(Console.ReadLine()) 
+        yield numb
+    ]
+
+//Функция нахождения первой цифры числа
+let First_Digit input =
+    let abs_input = abs input
+    let rec Get_First_Digit numb =
+        if numb<10 then
+            numb
+        else Get_First_Digit (numb/10)
+    Get_First_Digit abs_input
+
+
+
+[<EntryPoint>]
+let main _ =
+    printfn "Программа создает список из первых цифр числа "
+    printf "Введите количество чисел: "
+    let n = int(Console.ReadLine()) 
+    if n <=0 then printf "Неверный ввод"
+    let List_of_Numbs = (input n)
+    let List_of_Difits = List.map First_Digit (List_of_Numbs)
+    printfn "Исходный список: %A" List_of_Numbs
+    printfn "Cписок первых цифр из чисел исходного списка: %A" List_of_Difits
+    0
---
+++ lab2.2.fs
@@ -0,0 +1,31 @@
+﻿open System
+
+//Сколько раз в списке встречается заданное число? 
+
+// Функция ввода
+let input N = [ 
+    for i in 1..N do
+        printf "Введите число: "
+        let numb = float(Console.ReadLine())
+        yield numb
+    ]
+
+
+let search list: int =
+    printf "Введите искомое число: "
+    let find_num = float(Console.ReadLine())
+    let quantity_of_occ = List.fold(fun coun elemen_of_list -> 
+        if elemen_of_list = find_num then coun+1 else coun) 0 list
+    quantity_of_occ
+
+[<EntryPoint>]
+let main _ =
+    printfn "Программа находит количество вхождений определнного числа в список "
+    printf "Введите количество чисел: "
+    let n = int(Console.ReadLine()) 
+    if n <=0 then printf "Неверный ввод"
+    let List_of_Numbs = (input n)
+    let result = search List_of_Numbs
+    printfn "Список для поиска: %A" List_of_Numbs
+    printf "Количество совпадений: %i" result
+    0
